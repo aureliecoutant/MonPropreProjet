86012d3c4c8c7a1a0dd85c29bd8b3679f872e15b
aurelie <aurelie.coutant@maif.fr>
2017-05-09 10:56:43 +0200

    modif script

--- script.sh
+++ script.sh
@@ -3,10 +3,12 @@ MonAutreFichier="MonAutreFichier"
 Version=sudo cat /proc/version
 
 echo $Version > $MonFichier
-if [$Version=~ "fedora"] then
-   yum list | grep "ssh" > $MonFichier;
-elif [$Version=~ "debian"] then
-   dpkg --list | grep "ssh" > $MonFichier;
+if [$Version=~ "fedora"]
+ then
+   yum list | grep "ssh" > $MonFichier
+elif [$Version=~ "debian"]
+ then
+   dpkg --list | grep "ssh" > $MonFichier
 fi
 yum list > $MonAutreFichier
 
